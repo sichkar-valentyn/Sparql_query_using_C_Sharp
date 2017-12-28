5e20922c114ac2b23a89de99f5ac1e29cb1c354d
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2017-12-28 19:23:17 +0300

    Uploading files
    Adding files to the repository

---
+++ Practice_12_Sichkar_Valentyn/Practice_12_Sichkar_Valentyn/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Practice_12_Sichkar_Valentyn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(24, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Query1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(204, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Query2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(389, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Query3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 126);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(465, 175);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SpringGreen;
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(24, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.HotPink;
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(389, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 50);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Accident Factors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(189, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "What is bad weather?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(371, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dangerous situations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 401);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "C# Sparq Query Programming. Sichkar Valentyn, 13541/7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Practice_12_Sichkar_Valentyn/Practice_12_Sichkar_Valentyn/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+
+namespace Practice_12_Sichkar_Valentyn
+{
+    public partial class Form1 : Form
+    {
+
+        private const string Query1 = @"
+PREFIX my: <http://www.codeproject.com/KB/recipes/n3_notation#>
+PREFIX ns0: <http://car-assistant.ru/description#>
+SELECT ?name
+WHERE {
+?name a ns0:AccidentFactors.
+}";
+
+        private const string Query2 = @"
+PREFIX my: <http://www.codeproject.com/KB/recipes/n3_notation#>
+PREFIX ns0: <http://car-assistant.ru/description#>
+SELECT ?name
+WHERE {
+ns0:BadWeather ns0:ConsistOf ?name.
+}";
+
+        private const string Query3 = @"
+PREFIX my: <http://www.codeproject.com/KB/recipes/n3_notation#>
+PREFIX ns0: <http://car-assistant.ru/description#>
+SELECT ?name
+WHERE {
+?name a ns0:DangerousSituations.
+}";
+
+        void sparqgSend(string Factors, string description)
+        {
+            var parser = new Notation3Parser();
+            var graph = new Graph();
+
+            //Console.WriteLine("Loading Notation-3 file.");
+            richTextBox1.Text += "Loading Notation-3 file:" + Environment.NewLine;
+            richTextBox1.Text += "N3_Sichkar_Valentyn.n3" + Environment.NewLine;
+            richTextBox1.Text += Environment.NewLine;
+            richTextBox1.Text += Environment.NewLine;
+            parser.Load(graph, @"n3\N3_Sichkar_Valentyn.n3");
+
+            //Console.WriteLine("Nodes:");
+            richTextBox1.Text += "Nodes:" + Environment.NewLine;
+            foreach (Triple triple in graph.Triples)
+            {
+                richTextBox1.Text += GetNodeString(triple.Subject) + GetNodeString(triple.Predicate) + GetNodeString(triple.Object) + Environment.NewLine;
+                //Console.WriteLine("{0} {1} {2}", GetNodeString(triple.Subject), GetNodeString(triple.Predicate), GetNodeString(triple.Object));
+            }
+
+            //Console.WriteLine();
+            richTextBox1.Text += Environment.NewLine;
+            //Console.WriteLine();
+            richTextBox1.Text += Environment.NewLine;
+
+            SparqlResultSet resultSet = graph.ExecuteQuery(Factors) as SparqlResultSet;
+            if (resultSet != null)
+            {
+                //Console.WriteLine("Querying results for variable 'name':");
+                richTextBox1.Text += description + Environment.NewLine;
+                for (int i = 0; i < resultSet.Count; i++)
+                {
+                    SparqlResult result = resultSet[i];
+                    //Console.WriteLine("{0}. {1}", i + 1, result["name"]);
+                    richTextBox1.Text += Convert.ToString(i + 1) + ". " + Convert.ToString(result["name"]) + Environment.NewLine;
+                }
+            }
+        }
+
+        static string GetNodeString(INode node)
+        {
+            string s = node.ToString();
+            switch (node.NodeType)
+            {
+                case NodeType.Uri:
+                    int lio = s.LastIndexOf('#');
+                    if (lio == -1)
+                        return s;
+                    else
+                        return s.Substring(lio + 1);
+                case NodeType.Literal:
+                    return string.Format("\"{0}\"", s);
+                default:
+                    return s;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sparqgSend(Query1, "Accident factors are:");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sparqgSend(Query2, "Bad weather might consist of:");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sparqgSend(Query3, "Dangerous situations are:");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
